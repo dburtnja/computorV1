802f18cd3ff949b3fc9d76f2bb2a0e478571b872
ezburde <denys.burtnyak@ericsson.com>
2018-10-17 16:10:01 +0300

    start computor V1

--- computor.py
+++ computor.py
@@ -3,6 +3,10 @@ from re import findall, search
 from pprint import pprint
 
 MAX_POLYNOMIAL_DEGREE = 2
+A = 2
+B = 1
+C = 0
+
 
 def usage(error=None):
     if error:
@@ -72,6 +76,8 @@ class Polynomial:
             [Term(term_str)
              for term_str in findall(r"[\-\+]?[\d\w\.\^]+", equation)]
 
+    def _add_dummy_terms(self):
+
     def simplify(self):
         terms_start = list(self._terms)
         temporary_terms = []
@@ -87,6 +93,7 @@ class Polynomial:
             temporary_terms = []
             self._terms.append(term)
         self._terms = sorted(self._terms)
+        self._add_dummy_terms()
         self._degree = self._terms[-1].get_power()
 
     def get_degree(self):
@@ -97,6 +104,51 @@ class Polynomial:
             return " ".join([str(term) for term in self._terms])
         else:
             return "0"
+
+    def get_term_coeff(self, index):
+        return self._terms[index].get_coefficient()
+
+    def find_discriminant(self):
+        return self.get_term_coeff(B) ** 2 - 4 * self.get_term_coeff(A) * self.get_term_coeff(C)
+
+    def _run_formula(self, sign):
+        a = self.get_term_coeff(A)
+        b = self.get_term_coeff(B)
+        c = self.get_term_coeff(C)
+        sqrt = lambda x: x**(1 / 2.0)
+
+        if sign == "+":
+            return (-b + sqrt(b**2 - 4 * a * c)) / (2 * a)
+        else:
+            return (-b - sqrt(b**2 - 4 * a * c)) / (2 * a)
+
+    def _check_zero_degree(self):
+        if self.get_term_coeff(0) == 0:
+            print("Many solutions available.")
+        else:
+            print("No solution available.")
+
+    def _check_one_degree(self):
+
+
+    def find_solution(self):
+        discriminant = self.find_discriminant()
+        degree = self.get_degree()
+        print(f"Polynomial discriminant: {discriminant}")
+        print(f"Polynomial degree: {self.get_degree()}")
+        if degree > MAX_POLYNOMIAL_DEGREE:
+            print(f"The polynomial degree is strictly grader then "
+                  f"{MAX_POLYNOMIAL_DEGREE}, I can't solve.")
+        elif degree == 0:
+            self._check_zero_degree()
+        elif degree == 1:
+            self._check_one_degree()
+        elif degree == 2:
+            if discriminant > 0:
+                return self._run_formula("-"), self._run_formula("+")
+            elif discriminant == 0:
+                return self._run_formula("+")
+        return ()
 
 
 class PolynomialEquation:
@@ -124,17 +176,21 @@ class PolynomialEquation:
     def __str__(self):
         return f"{self._left_polynomial} = {self._right_polynomial}"
 
+    def find_solution(self):
+        return self._left_polynomial.find_solution()
+
 
 if __name__ == '__main__':
     if len(argv) != 2:
         usage("Invalid number of arguments.")
     polynomial_equation = PolynomialEquation(argv[1])
-    # print(polynomial_equation)
     polynomial_equation.simplify()
     print(f"Reduced form: {polynomial_equation}")
-    print(f"Polynomial degree: {polynomial_equation.get_degree()}")
-    if polynomial_equation.get_degree() > MAX_POLYNOMIAL_DEGREE:
-        print(f"The polynomial degree is strictly grader then "
-              f"{MAX_POLYNOMIAL_DEGREE}, I can't solve.")
-
+    solution = polynomial_equation.find_solution()
+    if len(solution) == 2:
+        print("Discriminant is strictly positive, the two solutions are:")
+        print(solution[0])
+        print(solution[1])
+    elif len(solution) == 1:
+        print(solution[0])
 
